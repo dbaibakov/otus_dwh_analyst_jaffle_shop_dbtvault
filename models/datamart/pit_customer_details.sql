0f296645e92b7afaef5e7a6885bb9f9d27a8596f
Áàéáàêîâ Äìèòðèé Âëàäèìèðîâè÷ <dbaibakov@gmail.com>
2022-07-17 02:24:09 +0300

    fix null values logic in PIT

--- models/datamart/pit_customer_details.sql
+++ models/datamart/pit_customer_details.sql
@@ -14,6 +14,7 @@ with raw as (
         , null as age
         , effective_from
         , customer_pk
+        , 1 as source
     from {{ ref('sat_customer_details') }}
     union all
     select
@@ -24,6 +25,7 @@ with raw as (
         , age
         , effective_from
         , customer_pk
+        , 2 as source
     from {{ ref('sat_customer_details_crm') }}
 )
 , collapsed as (
@@ -35,17 +37,18 @@ with raw as (
         , max(age) as age
         , effective_from
         , customer_pk
+        , bit_or(source) as source
     from raw
     group by effective_from, customer_pk
 )
 
 select
     customer_pk
-    , coalesce(first_name, lag(first_name) over (partition by customer_pk order by effective_from)) as first_name
-    , coalesce(last_name, lag(last_name) over (partition by customer_pk order by effective_from)) as last_name
-    , coalesce(email, lag(email) over (partition by customer_pk order by effective_from)) as email
-    , coalesce(country, lag(country) over (partition by customer_pk order by effective_from)) as country
-    , coalesce(age, lag(age) over (partition by customer_pk order by effective_from)) as age
+    , coalesce(first_name, case when source & 1 = 0 then lag(first_name) over (partition by customer_pk order by effective_from) end) as first_name
+    , coalesce(last_name, case when source & 1 = 0 then lag(last_name) over (partition by customer_pk order by effective_from) end) as last_name
+    , coalesce(email, case when source & 1 = 0 then lag(email) over (partition by customer_pk order by effective_from) end) as email
+    , coalesce(country, case when source & 2 = 0 then lag(country) over (partition by customer_pk order by effective_from) end) as country
+    , coalesce(age, case when source & 2 = 0 then lag(age) over (partition by customer_pk order by effective_from) end) as age
     , effective_from
     , coalesce(lead(effective_from) over (partition by customer_pk order by effective_from), '99991231') as effective_to
 from collapsed
